868f9ea573b445040bdcd21933451fa0d345efc6
Corey Daley <cdaley@missiondata.com>
2012-05-04 14:27:37 -0400

    adding help file

--- lib/seeds/generate_seeds.rb
+++ lib/seeds/generate_seeds.rb
@@ -1,25 +1,27 @@
 module Seeds
-  def self.generate_seeds
-    @@models_to_dump.each do |model_name, truefalse|
-      seeds = []
-      seeds << "#Dumping model [#{model_name}]"
-      begin
-        model_name.constantize.all.each do |r|
-          unless r.to_s == 'SchemaMigrations'
-              attributes_hash = []
-              r.attributes.collect do |attr|
-                unless @@drop_fields.size > 0 && @@drop_fields.include?(attr[0])
-                  attributes_hash << ":#{attr[0]} => %q[#{attr[1]}]"
+  class Sow
+    def self.generate_seeds
+      @@models_to_dump.each do |model_name, truefalse|
+        seeds = []
+        seeds << "#Dumping model [#{model_name}]"
+        begin
+          model_name.constantize.all.each do |r|
+            unless r.to_s == 'SchemaMigrations'
+                attributes_hash = []
+                r.attributes.collect do |attr|
+                  unless @@drop_fields.size > 0 && @@drop_fields.include?(attr[0])
+                    attributes_hash << ":#{attr[0]} => %q[#{attr[1]}]"
+                  end
                 end
-              end
-              seeds << "#{model_name}.create(#{attributes_hash.join(", ")})"
-          end 
-        end  
-      rescue Exception => e
-        puts e
+                seeds << "#{model_name}.create(#{attributes_hash.join(", ")})"
+            end 
+          end  
+        rescue Exception => e
+          puts e
+        end
+        seeds << "#Finished dumping model [#{model_name}] Records:#{seeds.size - 1}"
+        File.open("#{Rails.root}/db/seeds.rb", "a"){ |f| seeds.each{ |s| f.puts s} }
       end
-      seeds << "#Finished dumping model [#{model_name}] Records:#{seeds.size - 1}"
-      File.open("#{Rails.root}/db/seeds.rb", "a"){ |f| seeds.each{ |s| f.puts s} }
     end
   end
 end
---
+++ lib/seeds/help.rb
@@ -0,0 +1,7 @@
+module Seeds
+  class Sow
+    def help
+      puts "SHOW ME SOME HELP!"
+    end
+  end
+end
